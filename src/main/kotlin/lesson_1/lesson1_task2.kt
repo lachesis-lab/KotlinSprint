f5154413555f58351584de33b18e77dc0a0d4a02
Lachesis <mgmmail@mail.ru>
2026-02-19 17:15:00 +0300

    KS-1-2 operate with variables variant

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -4,8 +4,9 @@ fun main() {
     val numOfOrders: Int = 75
     val gratitudeForPurchase: String = "Благодарим за покупку"
     var numOfEmpl: Int = 2000
-//    println(numOfOrders)
-//    println(gratitudeForPurchase)
+    println(numOfOrders)
+    println(gratitudeForPurchase)
+//    println(numOfEmpl)
     numOfEmpl--
     println(numOfEmpl)
 
