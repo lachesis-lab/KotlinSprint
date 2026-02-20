3898f3e423e3908a15f71c228217f64b9620eb6e
Lachesis <mgmmail@mail.ru>
2026-02-20 12:17:02 +0300

    KS-1-5 print Gagarin's flight time fix

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_1
+const val secondPerMinute:Int = 60
 
 fun main(){
-    val seconds: Int = 6480
-    var hours: Int
-    var minutes: Int
-    var remMinutes: Int
-    var remSeconds: Int
+    val seconds = 6481
 
-    hours = seconds / 3600
-    remSeconds = seconds - hours * 3600
-    minutes = remSeconds / 60
-    remSeconds = seconds - hours * 3600- minutes * 60
+    val hours = seconds / (secondPerMinute*secondPerMinute)
+    var remSeconds = seconds - hours * secondPerMinute*secondPerMinute
+    val minutes = remSeconds / secondPerMinute
+    remSeconds = seconds - hours * secondPerMinute*secondPerMinute- minutes * secondPerMinute
 
     println("%02d:%02d:%02d".format(hours, minutes,remSeconds))
 }
